5d7a6c1e74a02a912030d9ea2d11c8f3982435ce
dharam-in <dharamdotin@gmail.com>
2024-06-26 17:51:41 +0530

    Project Basic Setup

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView, Platform, StatusBar } from 'react-native'
+import React from 'react'
+import Main from './Main'
+import { Link } from '@react-navigation/native'
+
+const App = () => {
+  return (
+    <Main />
+  )
+}
+
+export default App
